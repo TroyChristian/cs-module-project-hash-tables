c5db060bb78863fa3bf692371aefca3b46c6ea2a
Troy Christian <apostasy8@gmail.com>
2020-07-08 17:18:11 -0400

    Add psuedo-code to Word Counter

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,14 @@
-def word_count(s):
-    # Your code here
+""" def word_count(s):
+        words.join(" ")
+    tally = {}
+
+    for letter in words:
+        if letter not in tally:
+            tally[letter] = 1
+
+        else:
+            tally[letter] +=1
+     """
 
 
 
@@ -8,3 +17,5 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+
+
