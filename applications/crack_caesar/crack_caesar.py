b006d9648aa98a8c91d6eb32fda18facd7325f16
Troy Christian <apostasy8@gmail.com>
2020-07-08 17:19:54 -0400

    add uper scratchpad to Crack Ceaser

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,11 @@
 # Use frequency analysis to find the key to ciphertext.txt, and then
 # decode it.
+#https://en.wikipedia.org/wiki/Frequency_analysis
+#Ceaser Uper:
+#Understand: 
+#Plan
+#Execute
+#Reflect
 
 # Your code here
 
