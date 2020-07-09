37c65ca9f8c00a0a11d47507cb22b01cd60c7eae
Troy Christian <apostasy8@gmail.com>
2020-07-09 18:27:07 -0400

    word count complete

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -2,19 +2,27 @@ import string
 ascii_letters = set(map(ord, string.ascii_letters))
 non_letters = ''.join(chr(i) for i in range(256) if i not in ascii_letters)
 
+ignored_characters = [" : ; , . - + = / \ | [ ] { } ( ) * ^ &"]
+
 
 def word_count(words):
+ 
     word_list = words.split()
     word_map = {}
 
     for i in word_list:
-        i.strip(non_letters)
-        if i in word_map:
-                word_map[i] += 1 # increment value of wordKey if it already exists in word map
-        elif i not in word_map:
-                word_map[i] = 1 # create key:value in list {I:1}
+        clean_word = i.strip(non_letters).lower() 
+        if clean_word in word_map:
+                word_map[clean_word] += 1 # increment value of wordKey if it already exists in word map
+        elif clean_word not in word_map:
+                word_map[clean_word] = 1 # create key:value in list {I:1}
                 print(word_map)
 
+        
+
+    return word_map
+
+ 
         
 
     return word_map
